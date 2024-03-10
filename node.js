ddbad1d85d13479b1fc9fd81e3b3f9850154b64f
scarlett <scarlettshen1207@gmail.com>
2024-03-10 17:06:34 +0800

    Third commitment

--- node.js
+++ node.js
@@ -1,17 +1,38 @@
+function RandomNumber(arr){
+  return Math.floor (Math.random()*arr)
+ }
+ 
+ 
+ const Message = {
+   Zodiacs:['Virgo','Sagittarius','Libra'],
+   Months: ['Jan','Feb','March'],
+   Fortunes: ['Your sign is sun.','Your intellect and quick-thinking are impressive, I always learn something new from you.','Your creativity is inspiring, I love seeing what you come up with.'],
+ }
+ 
+ 
+ for (let prop in Message){
+ 
+   let OptionIndex = RandomNumber(Message[prop].length)
+ 
+    switch(prop) {
 
+    case 'Zodiacs':
 
-let message = {
-  Zodiacs:['Virgo','Sagittarius','Libra'],
-  Months: ['Jan','Feb','March'],
-  Messages: ['Your sign is sun.','Your intellect and quick-thinking are impressive, I always learn something new from you.','Your creativity is inspiring, I love seeing what you come up with.']
+    console.log (`${Message[prop][OptionIndex]}`)
+
+    break
+
+    case 'Months':
+
+    console.log (`${Message[prop][OptionIndex]}`)
+
+    break
+
+    case 'Fortunes':
+
+    console.log (`${Message[prop][OptionIndex]}`)
+
+    }
+
 }
 
-let  randomessage = ()=>{
-  let Zodiac= message.Zodiac[Math.floor (Math.random()*3)]
-  let Month= message.Zodiac[Math.floor (Math.random()*3)]
-  let Message= message.Zodiac[Math.floor (Math.random()*3)]
-
-  console.log (`${Zodiac}+'in' ${Month}+'Here is your fortune:'+${Message}`)
-}
-
-randomessage()
